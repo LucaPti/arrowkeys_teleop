19367ccf3025d84fc3cf4a27e45bac85102b2243
Luca <l.puccetti@gmail.com>
2020-06-28 17:06:05 +0200

    Experimental code for raw keyboard input

--- src/arrowkeys_teleop_node.cpp
+++ src/arrowkeys_teleop_node.cpp
@@ -2,6 +2,15 @@
 #include <geometry_msgs/Twist.h>
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <time.h>
+#include <stdint.h>
+// ioctl stuff runs only as root -> sudo -s before rosrun
+
 int main(int argc, char **argv)
 {
   //Initiate ROS
@@ -9,12 +18,31 @@ int main(int argc, char **argv)
   ros::NodeHandle n_; 
   ros::Publisher pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   ROS_INFO("publishing to cmd_vel");
-
   ros::Rate loop_rate(100);
 
-  while (ros::ok())
+  // Keyboard stuff
+  int rcode = 0;
+
+  char keyboard_name[256] = "Unknown";
+  int keyboard_fd = open("/dev/input/event1", O_RDONLY | O_NONBLOCK);
+  if ( keyboard_fd == -1 ) {
+    printf("Failed to open keyboard.\n");
+    exit(1);
+  }
+  rcode = ioctl(keyboard_fd, EVIOCGNAME(sizeof(keyboard_name)), keyboard_name);
+  printf("Reading From : %s \n", keyboard_name);
+
+  printf("Getting exclusive access: ");
+  rcode = ioctl(keyboard_fd, EVIOCGRAB, 1);
+  printf("%s\n", (rcode == 0) ? "SUCCESS" : "FAILURE");
+  struct input_event keyboard_event;
+int end = time(NULL) + 10;
+// Goal: detect arrow key to get exclusive access, esc restores old mode
+  while (ros::ok() && (time(NULL)<end))
   {
-    
+    if ( read(keyboard_fd, &keyboard_event, sizeof(keyboard_event)) != -1 ) {
+      printf("keyboard event: type %d code %d value %d  \n", keyboard_event.type, keyboard_event.code, keyboard_event.value);
+    }
     geometry_msgs::Twist msg;
     msg.linear.x = 0.25;
     msg.angular.z = 0.1;
@@ -24,5 +52,8 @@ int main(int argc, char **argv)
 
     loop_rate.sleep();
   }
+printf("Exiting.\n");
+  rcode = ioctl(keyboard_fd, EVIOCGRAB, 1);
+  close(keyboard_fd);
   return 0;
 }
